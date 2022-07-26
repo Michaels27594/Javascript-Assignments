e159c46241d683c0bb737073700af4bd807a2acc
Michael Sanchez <96930354+MikeSanDev@users.noreply.github.com>
2022-07-26 16:08:16 -0500

    Likes Assignment
    Javascript project - adding value to a string and increasing it with a button. got to learn how document.querySelector worked.

---
+++ likes.js
@@ -0,0 +1,39 @@
+
+
+// BTN 1
+var like = 9;
+var likeElement = document.querySelector("#like1")
+console.log(likeElement)
+
+function ninjaLike() {
+    like++;
+    likeElement.innerText = like + " like(s)";
+    console.log(like);
+
+}
+
+
+// BTN 2
+var like2 = 12;
+var likeElement2 = document.querySelector("#like2")
+console.log(likeElement2)
+
+function ninjaLike2() {
+    like2++;
+    likeElement2.innerText = like2 + " like(s)";
+    console.log(like2);
+
+}
+
+
+// BTN 3
+var like3 = 9;
+var likeElement3 = document.querySelector("#like3")
+console.log(likeElement3)
+
+function ninjaLike3() {
+    like3++;
+    likeElement3.innerText = like3 + " like(s)";
+    console.log(like3);
+
+}
